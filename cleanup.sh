d9a4a56a2be551e36733ff9a4da20f2bf72854a8
DylanWu <dw@watelier.cn>
2017-08-01 21:31:34 +0800

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -6,10 +6,10 @@ IFS=,
 for i in $DOMAIN
 do
   FOLDER="/etc/letsencrypt/live/$i"
-  CHAIN="$FOLDER/chain.pem"
-  CRT="$FOLDER/fullchain.pem"
-  KEY="$FOLDER/key.pem"
-  cp -s $CHAIN $i.chain.pem
-  cp -s $CRT $i.crt
-  cp -s $KEY $i.key
+  CHAIN="${FOLDER}/chain.pem"
+  CRT="${FOLDER}/fullchain.pem"
+  KEY="${FOLDER}key.pem"
+  cp -s $CHAIN ${i}.chain.pem
+  cp -s $CRT ${i}.crt
+  cp -s $KEY ${i}.key
 done
